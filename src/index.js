9f907118b6a044de5953dd64ae0a21ec7091434d
iwillteixeira <iwill2005@gmail.com>
2021-11-30 17:06:54 -0300

    Fix Linter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import './scss/style.scss';
-import { firstDisplay } from './jsmodules/array.js';
+import firstDisplay from './jsmodules/array.js';
 
 console.log(firstDisplay);
--- src/jsmodules/array.js
+++ src/jsmodules/array.js
@@ -1,6 +1,7 @@
-export const firstDisplay = [
+const firstDisplay = [
   'Wash the dishes',
   'Build a To-Do app',
   'dont\' forget about the webpack',
-
 ];
+
+export default firstDisplay;
