23964917043cf5880f4484b7d78f26cbdf4078c7
iwillteixeira <iwill2005@gmail.com>
2021-11-30 16:38:53 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import 'bootstrap'
-import './scss/main.scss';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import './scss/style.scss';
+import { firstDisplay } from './jsmodules/array.js';
+
+console.log(firstDisplay);
---
+++ src/jsmodules/array.js
@@ -0,0 +1,6 @@
+export const firstDisplay = [
+  'Wash the dishes',
+  'Build a To-Do app',
+  'dont\' forget about the webpack',
+
+];
